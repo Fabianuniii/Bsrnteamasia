014463a8c1cc4b165eace32861daa635903571c4
Fabianuniii <fmatzollek@gmail.com>
2025-06-22 00:24:45 +0200

    einrückungen angepasst

--- bash_start.sh
+++ bash_start.sh
@@ -6,19 +6,19 @@ echo "Starte Chat-System ..."
 cp config_wsl.toml config.toml
 
 # 2. Broadcast-Server starten
-echo "  Starte Broadcast-Server ..."
+echo "Starte Broadcast-Server ..."
 python3 broadcast_server.py &
 
 # 3. Clients starten: Index 2 und 3 aus config.toml
-echo " Starte Clients 2 und 3 ..."
+echo "Starte Clients 2 und 3 ..."
 python3 client.py 2 &
 python3 client.py 3 &
 
 # 4. Hinweis für CLI-Start
 echo ""
-echo " Alle Hintergrundprozesse laufen nun."
-echo " Bitte öffne zwei neue Terminals und führe jeweils folgendes aus:"
+echo "Alle Hintergrundprozesse laufen nun."
+echo "Bitte öffne zwei neue Terminals und führe jeweils folgendes aus:"
 echo "    python3 cli.py 2"
 echo "    python3 cli.py 3"
 echo ""
-echo " Danach kannst du direkt im CLI Befehle wie 'JOIN', 'MSG', 'AWAY', 'WHO' usw. eingeben."
+echo "Danach kannst du direkt im CLI Befehle wie 'JOIN', 'MSG', 'AWAY', 'WHO' usw. eingeben."
