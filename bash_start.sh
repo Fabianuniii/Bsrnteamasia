33d71d366e8661068dd8a1472fbfae89b4477686
Fabianuniii <fmatzollek@gmail.com>
2025-06-22 00:56:30 +0200

    text in bash verändert

--- bash_start.sh
+++ bash_start.sh
@@ -9,15 +9,17 @@ cp config_wsl.toml config.toml
 echo "Starte Broadcast-Server ..."
 python3 broadcast_server.py &
 
-# 3. Clients starten: Index 2 und 3 aus config.toml
-echo "Starte Clients 2 und 3 ..."
+# 3. Clients starten: Index 1, 2 und 3 aus config.toml
+echo " Starte Clients 2 und 3 ..."
+python3 client.py 1 &
 python3 client.py 2 &
 python3 client.py 3 &
 
 # 4. Hinweis für CLI-Start
 echo ""
 echo "Alle Hintergrundprozesse laufen nun."
-echo "Bitte öffne zwei neue Terminals und führe jeweils folgendes aus:"
+echo "Bitte öffne drei neue Terminals und führe jeweils folgendes aus:"
+echo "    python3 cli.py 1"
 echo "    python3 cli.py 2"
 echo "    python3 cli.py 3"
 echo ""
